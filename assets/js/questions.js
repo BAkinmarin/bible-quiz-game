e6a067058ec135e27d6d3b39c3de3fa14151b0de
Bola Akinmarin <0fd1c8c8@students.codeinstitute.net>
2024-06-18 11:36:26 +0000

    feat: Add JavaScript file to hold quiz questions array.

---
+++ assets/js/questions.js
@@ -0,0 +1,62 @@
+// Array of questions to be used in gameplay
+let questions = [
+    {
+        num: 1,
+        question: 'Which book in the Bible mentions a huge man with six fingers on each hand and six toes on each foot?',
+        answer: '"In still another battle, which took place at Gath, there was a huge man with six fingers on each hand and six toes on each foot-twenty-four in all. He also was descended from Rapha" - 2 Samuel 21:20(NIV)',
+        options: [
+            'Genesis',
+            'Samuel',
+            'Exodus',
+            'Revelation'
+        ]
+    },
+
+    {
+        num: 2,
+        question: 'Whose dead bones brought a man back to life again after the body touched them?',
+        answer: "Once while some Israelites were burying a man, suddenly they saw a band of raiders; so they threw the mans body into Elisha's tomb. When the body touched Elisha's bones, the man came to life and stood up on his feet. - 2 Kings 13:21 (NIV)",
+        options: [
+            'Eli',
+            'Moses',
+            'Joseph',
+            'Elisha'
+        ]
+    },
+
+    {
+        num: 3,
+        question: 'Which book in the Bible mentions a unicorn?',
+        answer: 'There are a few actually! Numbers 23:22, 24:8; Deuteronomy 33:17; Job 39:9-12; Psalm 22:21, 29:6, 92:10; Isaiah 34:7 (KJV)',
+        options: [
+            'Numbers',
+            'Deuteronomy',
+            'Job',
+            'All of the above'
+        ]
+    },
+
+    {
+        num: 4,
+        question: 'How tall does the Bible say Goliath was?',
+        answer: "Then Goliath, a Philistine champion from Gath, came out of the Philistine ranks to face the forces of Israel. He was over nine feet tall! - 1 Samuel 17:4 (NLT)",
+        options: [
+            'Over six feet tall',
+            'Over nine feet tall',
+            '15 feet tall',
+            '"As tall as three horses stacked on top of each other"'
+        ]
+    },
+
+    {
+        num: 5,
+        question: 'What does the Bible say John the Baptist ate?',
+        answer: "John's clothes were made of camel's hair, and he had a leather belt around his waist. His food was locusts and wild honey. - Matthew 3:4 (NIV)",
+        options: [
+            'Wafers and Milk',
+            'Fish and Bread',
+            'Figs and Wine',
+            'Locusts and Wild Honey'
+        ]
+    }
+]
