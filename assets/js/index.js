915467060ad0e0e77c19670ce630deb5d31b46c6
Bola Akinmarin <0fd1c8c8@students.codeinstitute.net>
2024-06-18 18:35:53 +0000

    style: Hide games and results boxes until click event.

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+//Getting all relevant elements from the DOM
+const quizRules = document.getElementById('quiz-rules');
+const startButton = document.getElementById('start-btn');
+
+//Add event listener to start game button display hidden game area and begin quiz
+
+
