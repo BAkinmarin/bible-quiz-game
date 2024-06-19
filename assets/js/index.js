99dadd245a6c65729b5fbc31c4770dcee6edafbc
Bola Akinmarin <0fd1c8c8@students.codeinstitute.net>
2024-06-19 16:48:56 +0000

    fix: Change code to hide and display relevant sections on start and exit.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,20 @@
-//Getting all relevant elements from the DOM
+//Get all relevant elements from the DOM
 const quizRules = document.getElementById('quiz-rules');
 const quizBox = document.getElementById('quiz-box');
 const startBtn = document.getElementById('start-btn');
 const exitBtn = document.getElementsByClassName('exit-btn');
+const questionSection = document.getElementsByClassName('question-section');
 
 function startQuiz() {
     alert("Goodluck!");
-    document.getElementById('quiz-rules').style.display = "none";
-    document.getElementById('quiz-box').style.display = "block";
+    quizRules.style.display = "none";
+    quizBox.style.display = "block";
 }
 
-
-
+function exitQuiz() {
+    alert("You are about to exit game!");
+    quizBox.style.display = "none";
+    quizRules.style.display = "block";
+}
 
 
