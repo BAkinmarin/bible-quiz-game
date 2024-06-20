4dfde07539274047c0fe5a88dd600c619b1ec32e
Bola Akinmarin <0fd1c8c8@students.codeinstitute.net>
2024-06-20 16:24:32 +0000

    feat: Recode functions declarations using Arrow syntax, restyle quiz area.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,39 +5,43 @@ const resultsBox = document.getElementById('results-box');
 const startBtn = document.getElementById('start-btn');
 const exitBtn = document.getElementsByClassName('exit-btn');
 const restartBtn = document.getElementsByClassName('restart-btn');
+const hiddenVerse = document.getElementById('hidden-verse');
 const question = document.getElementById('question');
 const optionText = Array.from(document.getElementsByClassName('option-text'));
-const questionTracker = document.getElementById('question-tracker');
+const questionsTracker = document.getElementById('questions-tracker');
 
 //Initialise score and question counter, create empty array for questions and empty object for current question
 let availableQuestions = [];
-let questionCounter = 0;
+let questionsCounter = 0;
 let score = 0;
 let currentQuestion = {};
 let acceptingAnswers = true;
-let clock = document.getElementById('clock');
-let timeLeft = parseInt(clock.innerText);
+//let clock = document.getElementById('clock');
+//let timeLeft = parseInt(clock.innerText);
 
 //Define fixed variables for max score and max number of questions
 const MAX_SCORE = 100;
 const MAX_QUESTIONS = 5;
 
-//Define startQuiz function to hide quiz rules and display quiz box
-function startQuiz() {
-    //alert("Goodluck!");
+/**
+ * Functions defined throughout this project will be defined using 'Arrow Function' syntax to simplify code.
+ * This idea was inspired by Brians Designs YouTube video tutorials.
+ */
+
+//Define startQuiz function to hide quiz rules, display quiz box and initialise score and questions counter
+    startQuiz = () => {
     quizRules.style.display = "none";
+    resultsBox.style.display = "none";
     quizBox.style.display = "block";
+    hiddenVerse.style.display = "none";
+    questionsCounter = 0;
     score = 0;
-    questionCounter = 0;
-    startTimer(15);
-    //Spread operator to get questions from array as seen on Brian's Design YouTube Channel
     availableQuestions = [...questions];
     getNewQuestion();
 }
 
 /**
  * The getNewQuestion function retrieves and displays questions and answer options at random from the array in questions.js file.
- * This function is defined using 'Arrow Function' syntax to simplify code.
  */
     getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionsCounter >= MAX_QUESTIONS) {
@@ -45,9 +49,15 @@ function startQuiz() {
         quizBox.style.display = "none";
     }
 
-    //Pick question at random from question array in questions.js file
-    questionCounter++;
-    questionTracker.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    //Increment questionCounter and display current question to user in corresponding HTML element
+    questionsCounter++;
+    questionsTracker.innerText = `Question ${questionsCounter} of ${MAX_QUESTIONS}`;
+
+    //Select question at random from question array in questions.js file and display in corresponding HTML element
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    //question.innerText = currentQuestion.question;
+    question.innerText = `Question ${questionsCounter}: ${currentQuestion.question}`;
 }
 
 //Define exitQuiz function to leave game and return to quiz rules page
