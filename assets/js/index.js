cedfa7fb09859ff2a826eb53dbf45b4c301e2fdd
Bola Akinmarin <0fd1c8c8@students.codeinstitute.net>
2024-06-20 18:08:06 +0000

    feat: Remove dead code.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,10 +5,12 @@ const resultsBox = document.getElementById('results-box');
 const startBtn = document.getElementById('start-btn');
 const exitBtn = document.getElementsByClassName('exit-btn');
 const restartBtn = document.getElementsByClassName('restart-btn');
+const nextBtn = document.getElementsByClassName('next-btn');
 const hiddenVerse = document.getElementById('hidden-verse');
 const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choices-text'));
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 const questionsTracker = document.getElementById('questions-tracker');
+const clock = document.getElementById('clock');
 
 //Initialise score and question counter, create empty array for questions and empty object for current question
 let availableQuestions = [];
@@ -16,12 +18,13 @@ let questionsCounter = 0;
 let score = 0;
 let currentQuestion = {};
 let acceptingAnswers = true;
-//let clock = document.getElementById('clock');
-//let timeLeft = parseInt(clock.innerText);
 
 //Define fixed variables for max score and max number of questions
 const MAX_SCORE = 100;
 const MAX_QUESTIONS = 5;
+
+//Define timer variable - inspired by Live Blogger - YouTube tutorial
+let timerInterval;
 
 /**
  * Functions defined throughout this project will be defined using 'Arrow Function' syntax to simplify code.
@@ -38,6 +41,7 @@ startQuiz = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    startTimer(15);
 }
 
 /**
@@ -68,6 +72,14 @@ getNewQuestion = () => {
     //Remove randomly displayed question from array and allow user to select answer
     availableQuestions.splice(questionsIndex, 1);
     acceptingAnswers = true;
+}
+
+/**
+ * This is the countdown timer function.
+ * It is passed a parameter of an integer which is the countdown value applied.
+ */
+startTimer = (time) => {
+
 }
 
 //Define exitQuiz function to leave game and return to quiz rules page  
